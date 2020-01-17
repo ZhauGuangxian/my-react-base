8d6722ecf953ff6b3467a9636b0d7d9ebfc2dad8
wanglinghui <wanglh@hua-cloud.com.cn>
2020-01-17 18:03:38 +0800

    feat: 用reactDom.createPortal实现一个对话框

---
+++ src/components/myModal/index.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import style from './modal.module.less';
+import { Button, Icon } from 'antd';
+function Portal(WrappedComponent) {
+    return class extends React.Component {
+        constructor(props) {
+            super(props);
+            if(!this.node) {
+                this.node = document.createElement('div');
+                document.body.append(this.node);
+            }
+        }
+        componentWillUnmount() {
+            if(this.node) {
+                this.node.remove();
+                this.node = null;
+            }
+        }
+        renderContext() {
+
+            return (
+                <div className={style['wrapper-container']}>
+                    <div className={style['wrapper-bg']} onClick={()=>{
+                        this.props.onCancel()
+                    }}></div>
+                    <WrappedComponent {...this.props}></WrappedComponent>
+                </div>
+            )
+        }
+        render() {
+            if(this.props.visible === true) {
+                return ReactDOM.createPortal(this.renderContext(), this.node)
+            } else {
+                return null;
+            }
+        }
+    }
+}
+
+class Modal extends React.Component {
+    constructor(props){
+        super(props)
+    }
+    render() {
+        let customfooter = false;
+        if(this.props.footer !== null && typeof this.props.footer !== 'undefined') {
+            customfooter = true;
+        }
+        let newStyle =  Object.assign(this.props.style || {}, {
+
+        })
+        if(!isNaN(parseInt(this.props.width))) {
+            newStyle['width'] = parseInt(this.props.width) + 'px'
+            
+        }
+        let newClassName = style['modal-container'];
+        if(this.props.className) {
+            newClassName = newClassName + ' ' + this.props.className
+        }
+        return (
+            <div className={newClassName} style={newStyle}>
+                <div className={style['modal-head']}>
+                    <span className={style['modal-head-title']}>{this.props.title || '标题'}</span>
+                    <Icon type="close" onClick={this.props.onCancel}/>
+                </div>
+                <div className={style['modal-body']}>
+                    {this.props.children}
+                </div>
+                {this.props.footer !== null && <div className={style['modal-footer']}>
+                    {
+                        customfooter === true ? this.props.footer : (<div>
+                            <Button onClick={this.props.onOk} type="primary" style={{marginRight: '10px'}}>{this.props.okText || '确定'}</Button>
+                            <Button onClick={this.props.onCancel} type="primary">{this.props.okText || '取消'}</Button>
+                        </div>)
+                    }
+                </div>}
+            </div>
+        )
+    }
+}
+
+export default Portal(Modal)
--- src/views/homePage/index.jsx
+++ src/views/homePage/index.jsx
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
-
+import { Button } from 'antd';
+import MyModal from '@/components/myModal/index';
 
 class HomePage extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            
+            myModalVisible: false
         };
     }
     
     render(){
         return(
-            <div>首页sasd1</div>
+            <div>
+                <div>
+                    首页
+                </div>
+                <div>
+                    <Button onClick={()=>{
+                        this.setState({
+                            myModalVisible: true
+                        })
+                    }}>试试modal</Button>
+                </div>
+                <MyModal visible={this.state.myModalVisible} onCancel={()=>{
+                    this.setState({
+                        myModalVisible: false
+                    })
+                }} title="reactCreatePortal" width="720">
+
+                    test reactDom creat potral
+                </MyModal>
+            </div>
         )
     }
 }
